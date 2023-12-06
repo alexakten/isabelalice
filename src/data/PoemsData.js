81f1790d54ba9b0baca86b378ff8e6671affb89b
Alex <alex.akten@outlook.com>
2023-12-07 09:47:11 +1100

    more poems

--- src/data/PoemsData.js
+++ src/data/PoemsData.js
@@ -2,28 +2,51 @@ const poems = [
   {
     id: 1,
     title: "my grandfather and home",
-    content: "my grandfather used to count the days on his fingers, \nmarking the rhythm of seasons and tides, \nin his eyes, a story of wisdom lingers, \nunderneath the sky where the seagull glides."
+    content:
+      "my grandfather used to count the days on his fingers, \nmarking the rhythm of seasons and tides, \nin his eyes, a story of wisdom lingers, \nunderneath the sky where the seagull glides.",
   },
   {
     id: 2,
     title: "whispers of the wind",
-    content: "whispers of the wind, so soft and slight, \ncarry dreams of day and secrets of night, \nover hills and valleys, they dance and they bend, \nin an endless waltz, with the leaves they blend."
+    content:
+      "whispers of the wind, so soft and slight, \ncarry dreams of day and secrets of night, \nover hills and valleys, they dance and they bend, \nin an endless waltz, with the leaves they blend.",
   },
   {
     id: 3,
     title: "the painter's dream",
-    content: "in the painter's dream, colors dare to fly, \nblues of oceans deep, yellows of the sky, \neach stroke tells a tale, bold or demure, \non the canvas of life, emotions pure."
+    content:
+      "in the painter's dream, colors dare to fly, \nblues of oceans deep, yellows of the sky, \neach stroke tells a tale, bold or demure, \non the canvas of life, emotions pure.",
   },
   {
     id: 4,
     title: "echoes in the forest",
-    content: "echoes in the forest, a haunting melody, \ntales of ancient times, and mysteries free, \ntrees stand as guardians of history, \nwhispering secrets, wrapped in mystery."
+    content:
+      "echoes in the forest, a haunting melody, \ntales of ancient times, and mysteries free, \ntrees stand as guardians of history, \nwhispering secrets, wrapped in mystery.",
   },
   {
     id: 5,
     title: "river of time",
-    content: "the river of time flows, relentless and deep, \ncarving paths in the earth, a course it must keep, \nin its waters, reflections of joy and of strife, \nmirroring the essence of each fleeting life."
-  }
+    content:
+      "the river of time flows, relentless and deep, \ncarving paths in the earth, a course it must keep, \nin its waters, reflections of joy and of strife, \nmirroring the essence of each fleeting life.",
+  },
+  {
+    id: 6,
+    title: "shadows and light",
+    content:
+      "in the dance of shadows and light, \nwhere dreams intertwine with night, \nwhispers of the past softly tread, \non paths where memories are silently led.",
+  },
+  {
+    id: 7,
+    title: "garden of thoughts",
+    content:
+      "in the garden of thoughts, ideas bloom, \nlike flowers in an endless room, \nfragrant with the scent of new-found creation, \na sanctuary of the mind's imagination.",
+  },
+  {
+    id: 8,
+    title: "ocean's embrace",
+    content:
+      "the ocean's embrace, wide and deep, \nholds secrets that the heavens keep, \nits waves tell stories, old and new, \nin shades of emerald and deepest blue.",
+  },
 ];
 
 export default poems;
