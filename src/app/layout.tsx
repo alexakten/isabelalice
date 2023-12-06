e4c5cd8474d3bce789e225baf97eea5634ba7f27
Alex <alex.akten@outlook.com>
2023-12-07 09:43:11 +1100

    updated description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Isabel Alice',
-  description: 'Author',
+  description: 'Author & Copywriter',
 }
 
 export default function RootLayout({
